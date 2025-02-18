a83aeaacf9235ceeda50a5fe9c4a215da4081cbc
Graham Roberts <graham.roberts@ucl.ac.uk>
2025-02-18 17:42:04 +0000

    Update dependencies, logging, and servlet imports for Jakarta EE
    Replaced `javax.servlet` with `jakarta.servlet` imports and upgraded dependencies in `pom.xml` to align with Jakarta EE standards. Enhanced `Main.java` with better logging, shutdown handling, and directory validation for a cleaner startup process. Updated project structure and configurations for compatibility with Tomcat 11 and Java 23.

--- src/main/java/uk/ac/ucl/main/Main.java
+++ src/main/java/uk/ac/ucl/main/Main.java
@@ -1,32 +1,108 @@
-package uk.ac.ucl.main;
+package ucl.ac.uk.main;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.*;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
-public class Main {
+public class Main
+{
+  private static final int DEFAULT_PORT = 8080;
+  private static final String DEFAULT_WEBAPP_DIR = "src/main/webapp/";
+  private static final String DEFAULT_TARGET_CLASSES = "target/classes";
+  private static final String WEB_INF_CLASSES = "/WEB-INF/classes";
+  private static final String LOGFILE = "logfile.txt";
 
-  public static void main(String[] args) throws Exception {
+  public static Thread addShutdown(final Tomcat tomcat, final Logger logger)
+  {
+    Thread shutdownHook = new Thread(() -> {
+      try
+      {
+        if (tomcat != null)
+        {
+          tomcat.stop();
+          tomcat.destroy();
+          logger.info("Tomcat has shut down normally.");
+        }
+      } catch (Exception e)
+      {
+        logger.log(Level.SEVERE, "Error shutting down Tomcat", e);
+      }
+    });
+    Runtime.getRuntime().addShutdownHook(shutdownHook);
+    return shutdownHook;
+  }
 
-    String webappDirLocation = "src/main/webapp/";
-    Tomcat tomcat = new Tomcat();
-    tomcat.setPort(8080);
+  private static Logger initialiseLogger()
+  {
+    Logger logger = Logger.getLogger(Main.class.getName());
 
-    tomcat.getConnector();
-    StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+    ConsoleHandler consoleHandler = new ConsoleHandler();
+    consoleHandler.setLevel(Level.INFO);
+    logger.addHandler(consoleHandler);
 
-    File additionWebInfClasses = new File("target/classes");
+    try {
+      FileHandler fileHandler = new FileHandler(LOGFILE);
+      fileHandler.setFormatter(new SimpleFormatter());
+      fileHandler.setLevel(Level.INFO);
+      logger.addHandler(fileHandler);
+    } catch (IOException e) {
+      logger.log(Level.SEVERE, "Failed to create log file", e);
+    }
 
-    WebResourceRoot resources = new StandardRoot(ctx);
-    resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
-      additionWebInfClasses.getAbsolutePath(), "/"));
-    ctx.setResources(resources);
+    logger.setLevel(Level.INFO);
+    return logger;
+  }
 
-    tomcat.start();
-    tomcat.getServer().await();
+  private static Context getContext(Path webappDirectory, Tomcat tomcat)
+  {
+    if (!Files.exists(webappDirectory) || !Files.isDirectory(webappDirectory))
+    {
+      throw new IllegalArgumentException("Webapp directory does not exist: " + webappDirectory);
+    }
+    return tomcat.addWebapp("/", webappDirectory.toAbsolutePath().toString());
+  }
+
+  private static void setResources(Context context, Path targetClassesDirectory)
+  {
+    WebResourceRoot resources = new StandardRoot(context);
+    resources.addPreResources(new DirResourceSet(resources, WEB_INF_CLASSES,
+      targetClassesDirectory.toAbsolutePath().toString(), "/"));
+    context.setResources(resources);
+  }
+
+  public static void main(String[] args)
+  {
+    final Logger logger = initialiseLogger();
+    final Path webappDirectory = Paths.get(DEFAULT_WEBAPP_DIR);
+    final Path targetClassesDirectory = Paths.get(DEFAULT_TARGET_CLASSES);
+    final Tomcat tomcat = new Tomcat();
+
+    try
+    {
+      tomcat.setPort(DEFAULT_PORT);
+      tomcat.getConnector();
+      addShutdown(tomcat, logger);
+
+      Context context = getContext(webappDirectory, tomcat);
+      setResources(context, targetClassesDirectory);
+
+      tomcat.start();
+      logger.info("Server started successfully on port " + DEFAULT_PORT);
+      tomcat.getServer().await();
+    } catch (IllegalArgumentException e)
+    {
+      logger.log(Level.SEVERE, "Configuration error", e);
+    } catch (Exception e)
+    {
+      logger.log(Level.SEVERE, "Error occurred while starting the server", e);
+    }
   }
 }
--- src/main/java/uk/ac/ucl/servlets/ViewPatientListServlet.java
+++ src/main/java/uk/ac/ucl/servlets/ViewPatientListServlet.java
@@ -1,15 +1,16 @@
 package uk.ac.ucl.servlets;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import uk.ac.ucl.model.Model;
 import uk.ac.ucl.model.ModelFactory;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.List;
 
