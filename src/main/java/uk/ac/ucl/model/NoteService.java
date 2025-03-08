7f6c5e7f21032091b5e43d1924da73621831efa7
Shuaiting Li <li.private0@gmail.com>
2025-03-08 22:51:30 +0000

    Integrate NoteFactory with NoteService

--- src/main/java/uk/ac/ucl/model/NoteService.java
+++ src/main/java/uk/ac/ucl/model/NoteService.java
@@ -2,11 +2,23 @@ package uk.ac.ucl.model;
 
 public class NoteService {
     private NoteRepository noteRepository;
+    private NoteFactory notefactory;
 
     public NoteService(NoteRepository noteRepository) {
         this.noteRepository = noteRepository;
+        this.notefactory = new NoteFactory(noteRepository);
     }
 
+    public NoteService(String dataPath) {
+        String indexFilePath = dataPath + "/index.json";
+        String noteFolderPath = dataPath + "/notes";
+        this.noteRepository = new JsonNoteRepository(indexFilePath, noteFolderPath);
+        this.notefactory = new NoteFactory(noteRepository);
+    }
+
+    public Note createNote() {
+        return notefactory.createNote();
+    }
 
     public Note getNoteById(String id) {
         return noteRepository.loadNoteById(id);
