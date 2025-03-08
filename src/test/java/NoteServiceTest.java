9fe891f4c50a3db55bfea3c229e1a08e603dabca
Shuaiting Li <li.private0@gmail.com>
2025-03-08 22:23:01 +0000

    update tests

---
+++ src/test/java/NoteServiceTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import uk.ac.ucl.model.JsonNoteRepository;
+import uk.ac.ucl.model.Note;
+import uk.ac.ucl.model.NoteService;
+
+public class NoteServiceTest {
+    @Test
+    public void testNoteServiceCreation() {
+        NoteService noteService = new NoteService(new JsonNoteRepository("temp/testIndex.json", "temp/notes/"));
+        assert noteService != null;
+    }
+
+    @Test
+    public void testNoteServiceAddNote() {
+        NoteService noteService = new NoteService(new JsonNoteRepository("temp/testIndex.json", "temp/notes/"));
+        noteService.addNote(new Note("2", "Test Note"));
+        noteService.addNote(new Note("3", "Test Note"));
+        Note note2 = noteService.getNoteById("2");
+        assert note2 != null;
+        assert note2.getId().equals("2");
+        assert note2.getTitle().equals("Test Note");
+    }
+
+    @Test
+    public void testNoteServiceUpdateNote() {
+        NoteService noteService = new NoteService(new JsonNoteRepository("temp/testIndex.json", "temp/notes/"));
+        Note note3 = noteService.getNoteById("3");
+        note3.setTitle("Updated Test Note");
+        noteService.updateNote(note3);
+        Note updatedNote3 = noteService.getNoteById("3");
+        assert updatedNote3 != null;
+        assert updatedNote3.getTitle().equals("Updated Test Note");
+    }
+
+    @Test
+    public void testNoteServiceGetNote() {
+        NoteService noteService = new NoteService(new JsonNoteRepository("temp/testIndex.json", "temp/notes/"));
+        Note note2 = noteService.getNoteById("2");
+        assert note2 != null;
+        assert note2.getId().equals("2");
+        assert !note2.getTitle().isEmpty();
+    }
+
+}
