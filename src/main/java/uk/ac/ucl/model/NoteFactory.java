fc372e07449cf7a3be95d619a3dcadd10f51baaa
Shuaiting Li <li.private0@gmail.com>
2025-03-08 22:35:08 +0000

    add NoteFactory

---
+++ src/main/java/uk/ac/ucl/model/NoteFactory.java
@@ -0,0 +1,30 @@
+package uk.ac.ucl.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NoteFactory {
+    private Set<String> noteIndex = new HashSet<String>();
+    private final NoteRepository noteRepository;
+
+    public NoteFactory(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+        noteIndex = noteRepository.getAllNoteIndex();
+    }
+
+    public Note createNote() {
+        String id = generateId();
+        Note note = new Note(id, "Untitled");
+        noteRepository.writeNote(note);
+        return note;
+    }
+
+    private String generateId() {
+        int id = 1;
+        while (noteIndex.contains(String.valueOf(id))) {
+            id++;
+        }
+        return String.valueOf(id);
+    }
+
+}
