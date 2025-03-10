907feaa79529bd3915ca3462f94290b76db33849
Shuaiting <109453496+spicyGrape@users.noreply.github.com>
2025-03-10 22:07:24 +0000

    Add save note functionality

--- src/main/java/uk/ac/ucl/servlets/SaveNoteServlet.java
+++ src/main/java/uk/ac/ucl/servlets/SaveNoteServlet.java
@@ -1,32 +1,22 @@
 package uk.ac.ucl.servlets;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import uk.ac.ucl.model.Note;
+import uk.ac.ucl.model.NoteContent;
 import uk.ac.ucl.model.NoteService;
 import uk.ac.ucl.model.NoteServiceFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-@WebServlet("/note.html")
-public class NoteServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        NoteService noteService = NoteServiceFactory.getNoteService();
-        String noteId = request.getParameter("noteId");
-        Note note = noteService.getNoteById(noteId);
-        request.setAttribute("note", note);
-
-        ServletContext context = getServletContext();
-        RequestDispatcher dispatch = context.getRequestDispatcher("/note.jsp");
-        dispatch.forward(request, response);
-    }
-
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+@WebServlet("/saveNote.html")
+public class SaveNoteServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         NoteService noteService = NoteServiceFactory.getNoteService();
         String noteId = request.getParameter("noteId");
         String noteTitle = request.getParameter("noteTitle");
